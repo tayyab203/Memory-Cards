a83a011c5e86d62c2ac4e2a9292ef6039edc2c5e
tayyab203 <61288485+tayyab203@users.noreply.github.com>
2020-05-03 11:43:22 +0500

    commit 8

--- script.js
+++ script.js
@@ -18,3 +18,47 @@ const cardsEl = [];
 
 // Store card data
 const cardsData = getCardsData();
+
+// const cardsData = [
+//   {
+//     question: 'What must a variable begin with?',
+//     answer: 'A letter, $ or _'
+//   },
+//   {
+//     question: 'What is a variable?',
+//     answer: 'Container for a piece of data'
+//   },
+//   {
+//     question: 'Example of Case Sensitive Variable',
+//     answer: 'thisIsAVariable'
+//   }
+// ];
+
+// Create all cards
+function createCards() {
+    cardsData.forEach((data, index) => createCard(data, index));
+  }
+  
+  // Create a single card in DOM
+  function createCard(data, index) {
+    const card = document.createElement('div');
+    card.classList.add('card');
+  
+    if (index === 0) {
+      card.classList.add('active');
+    }
+  
+    card.innerHTML = `
+    <div class="inner-card">
+    <div class="inner-card-front">
+      <p>
+        ${data.question}
+      </p>
+    </div>
+    <div class="inner-card-back">
+      <p>
+        ${data.answer}
+      </p>
+    </div>
+  </div>
+    `;
